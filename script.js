ebc4f0bf5c0671a3546dda68f2d7d76c67673bea
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-15 22:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
     //Write your code here
+    let elementCollection = document.getElementsByTagName("strong");
+	
+     let elementArr = Array.from(elementCollection);
 
-
+	elementArr.forEach((element)=>{
+		element.style.color = "green";
+	});
 }
 
 
 function return_normal() {
     //Write your code here
+	let elementCollection = document.getElementsByTagName("strong");
+	
+     let elementArr = Array.from(elementCollection);
+
+	elementArr.forEach((element)=>{
+		element.style.color = "black";
+	});
 
     
 }
